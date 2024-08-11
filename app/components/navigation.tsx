a27b3725fde7ec82c142e6a5820183fdfc2bc8b6
Abby Bashore <abbyjbdev@gmail.com>
2024-08-11 11:40:08 -0700

    resize svg icons using tailwind classes
    https://github.com/gregberge/svgr/discussions/826

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -8,10 +8,10 @@ export default function Navigation() {
         <h1 className="md:text-7xl text-5xl text-nowrap">Abigail Bashore</h1>
         <nav className="sm:flex hidden">
           <Link className="m-2" target="_blank" href={"https://www.linkedin.com/in/abigail-bashore"}>
-            <LinkedIn />
+            <LinkedIn className={"md:size-16 md:leading-none size-10"} />
           </Link>
           <Link className="m-2" target="_blank" href={"https://www.github.com/abbyjb"}>
-            <Github />
+            <Github className={"md:size-16 md:leading-none size-10"} />
           </Link>
         </nav>
       </div>   
