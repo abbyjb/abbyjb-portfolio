5f9bf1246fdac41426f6b6063223a39b3c8f1d9b
Abby Bashore <abbyjbdev@gmail.com>
2024-08-11 09:50:07 -0700

    create a color palette using coolors.co

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,8 @@ export default function Home() {
           height={300}
           alt="abby in nyc"
       />
-      <div className="p-2 m-2 h-fit w-fit col-span-3 rounded-lg bg-lightest-purple">
-        <p className="text-lg text-darkest-purple">
+      <div className="p-2 m-2 h-fit w-fit col-span-3 rounded-lg bg-off-white">
+        <p className="text-lg text-purple">
           Software engineer with over five years of experience in building maintainable and performant software solutions. 
           Strengths in delivering well architected and tested code. Provides concise and actionable updates. 
           Collaborates with cross-functional teams in a remote environment. 
