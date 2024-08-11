d028a0b9586efddc9842d5a73681a61562338706
Abby Bashore <abbyjbdev@gmail.com>
2024-08-11 13:01:29 -0700

    move navigation to layout so it renders on all routes

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -4,7 +4,7 @@ import LinkedIn from '@/app/linkedin.svg'
 
 export default function Navigation() {
   return (
-    <div className="flex align-text-bottom justify-between mb-5 col-span-4">
+    <div className="flex align-text-bottom justify-between my-5 col-span-4">
         <h1 className="lg:text-7xl text-5xl text-nowrap">Abigail Bashore</h1>
         <nav className="sm:flex hidden">
           <Link className="m-2" target="_blank" href={"https://www.linkedin.com/in/abigail-bashore"}>
