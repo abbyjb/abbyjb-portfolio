fbabfc9362bff1f4a5cee1698934c433f201a7a7
Abby Bashore <abbyjbdev@gmail.com>
2024-08-10 23:54:34 -0700

    start first iteration of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Github from './github.svg'
+import LinkedIn from './linkedin.svg'
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen w-[75vw] mx-auto">
-      <div className="p-5">
-        <h1 className="text-lg">Hello, world!</h1>
-      </div>
+    <main className="grid gap-3 grid-cols-4 grid-rows-layout min-h-screen w-[90vw] mx-auto my-5">
+      <div className="flex align-text-bottom justify-between mb-5 col-span-4">
+        <h1 className="text-7xl">Abigail Bashore</h1>
+        <nav className="flex">
+          <Link className="m-2" target="_blank" href={"https://www.linkedin.com/in/abigail-bashore"}>
+            <LinkedIn />
+          </Link>
+          <Link className="m-2" target="_blank" href={"https://www.github.com/abbyjb"}>
+            <Github />
+          </Link>
+        </nav>
+      </div>      
+      <Image
+          className="rounded-lg my-2"
+          src="/abbyjb-in-nyc.jpg"
+          width={300}
+          height={300}
+          alt="abby in nyc"
+      />
+      <div className="p-2 m-2 h-fit w-fit col-span-3 rounded-lg bg-lightest-purple">
+        <p className="text-lg text-darkest-purple">
+          Software engineer with over five years of experience in building maintainable and performant software solutions. 
+          Strengths in delivering well architected and tested code. Provides concise and actionable updates. 
+          Collaborates with cross-functional teams in a remote environment. 
+          Proficient in full-stack development in Ruby on Rails, React, PostgreSQL, and AWS.
+        </p>
+        </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "lightest-purple": "#efe6fe",
+        "darkest-purple": "#090119",
+      },
+      gridTemplateRows: {
+        'layout': '100px minmax(900px, 1fr) 100px',
+      }
     },
   },
   plugins: [],
