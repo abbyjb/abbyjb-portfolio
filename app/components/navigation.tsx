6cb6c763e9e5eb3a932c70d3339bda8bda902397
Abby Bashore <abbyjbdev@gmail.com>
2024-08-11 11:55:41 -0700

    tweak navigation sizing

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -5,13 +5,13 @@ import LinkedIn from '@/app/linkedin.svg'
 export default function Navigation() {
   return (
     <div className="flex align-text-bottom justify-between mb-5 col-span-4">
-        <h1 className="md:text-7xl text-5xl text-nowrap">Abigail Bashore</h1>
+        <h1 className="lg:text-7xl text-5xl text-nowrap">Abigail Bashore</h1>
         <nav className="sm:flex hidden">
           <Link className="m-2" target="_blank" href={"https://www.linkedin.com/in/abigail-bashore"}>
-            <LinkedIn className={"md:size-16 md:leading-none size-10"} />
+            <LinkedIn className={"lg:size-16 lg:leading-none size-10"} />
           </Link>
           <Link className="m-2" target="_blank" href={"https://www.github.com/abbyjb"}>
-            <Github className={"md:size-16 md:leading-none size-10"} />
+            <Github className={"lg:size-16 lg:leading-none size-10"} />
           </Link>
         </nav>
       </div>   
