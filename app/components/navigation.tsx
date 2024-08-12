87c09690c97e8a169b070615aad1260c7fb43316
Abby Bashore <abbyjbdev@gmail.com>
2024-08-11 19:38:56 -0700

    home page and navigation adjustments

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -4,16 +4,27 @@ import LinkedIn from '@/app/linkedin.svg'
 
 export default function Navigation() {
   return (
-    <div className="flex align-text-bottom justify-between my-5 col-span-4">
+    <nav className="flex sm:flex-row flex-col items-center justify-between my-7 ">
+      <Link className="grow" href="/">
         <h1 className="lg:text-7xl text-5xl text-nowrap">Abigail Bashore</h1>
-        <nav className="sm:flex hidden">
-          <Link className="m-2" target="_blank" href={"https://www.linkedin.com/in/abigail-bashore"}>
-            <LinkedIn className={"lg:size-16 lg:leading-none size-10"} />
-          </Link>
-          <Link className="m-2" target="_blank" href={"https://www.github.com/abbyjb"}>
-            <Github className={"lg:size-16 lg:leading-none size-10"} />
-          </Link>
-        </nav>
-      </div>   
+      </Link>
+      <div className="flex">
+        <Link className="lg:text-3xl lg:leading-none m-2 hover:transition-shadow hover:text-off-white hover:shadow-inner-md hover:bg-[#3A6669] hover:shadow-blue text-2xl rounded-lg p-2" href={"/experience"}>
+          Resume
+        </Link>
+        <Link className="lg:text-3xl lg:leading-none m-2 hover:transition-shadow hover:text-off-white hover:shadow-inner-md hover:bg-[#3A6669] hover:shadow-blue text-2xl rounded-lg p-2" href={"mailto:abbyjbdev@gmail.com"}>
+          Contact
+        </Link>
+      </div>
+      <div className="flex">
+        <Link className="m-2" target="_blank" href={"https://www.linkedin.com/in/abigail-bashore"}>
+          <LinkedIn className={"lg:size-9 lg:leading-none hover:transition-shadow hover:shadow-xl hover:shadow-blue size-6"} />
+        </Link>
+        <Link className="m-2" target="_blank" href={"https://www.github.com/abbyjb"}>
+          <Github className={"rounded-full lg:size-9 lg:leading-none hover:transition-shadow hover:shadow-xl hover:shadow-blue size-6"} />
+        </Link>  
+      </div>
+          
+    </nav>  
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col lg:flex-row items-start justify-between gap-3">
+    <main className="flex flex-col md:flex-row items-start justify-between gap-3">
       <Image
-          className="rounded-lg"
+          className="rounded-lg shadow-xl shadow-blue"
           src="/abbyjb-in-nyc.jpg"
           style={{
-            width: '50%',
+            width: '100%',
             height: 'auto',
           }}
           width={300}
           height={300}
           alt="abby in nyc"
       />
-      <div className="p-2 h-fit rounded-lg bg-lighter-beige">
+      <div className="p-2 rounded-lg bg-beige shadow-lg shadow-blue">
         <p className="text-lg text-purple">
           Software engineer with over five years of experience in building maintainable and performant software solutions. 
           Strengths in delivering well architected and tested code. Provides concise and actionable updates. 
