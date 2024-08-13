3df2e2562f18ef92fedd3f6b2346d6671b176b91
Abby Bashore <abbyjbdev@gmail.com>
2024-08-12 21:35:09 -0700

    more enhancements; add about section

--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,46 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="sm:grid sm:grid-cols-2 flex flex-col items-center justify-between gap-3">
-      <div className="relative">
+    <main>
+      <div className="sm:flex sm:flex-row items-center justify-between mb-3 gap-3">
         <Image
-          className="sm:absolute sm:top-0 sm:left-0 my-3 rounded-full shadow-xl shadow-blue"
-          src="/abbyjb-in-nyc.jpg"
-          // style={{
-          //   width: '100%',
-          //   height: 'auto',
-          // }}
-          width={300}
-          height={300}
-          alt="abby in nyc"
-        />
-        <Image 
-          className="sm:absolute sm:top-40 sm:left-40 my-3 rounded-full shadow-xl shadow-blue"
-          src="/abbyjb-running-5k.jpg"
-          // style={{
-          //   width: '100%',
-          //   height: 'auto',
-          // }}
-          width={300}
-          height={300}
-          alt="abby running in a 5k"
-        />
+            className="my-3 sm:rounded-lg rounded-full"
+            src="/abbyjb-in-nyc.jpg"
+            width={250}
+            height={250}
+            alt="abby in new york city"
+          />
+          <Image 
+            className="sm:block mx-auto hidden my-3 rounded-lg"
+            src="/abbyjb-running-5k.jpg"
+            width={250}
+            height={250}
+            alt="abby running a 5k race"
+          />
+          <Image 
+            className="sm:block hidden my-3 rounded-lg"
+            src="/abbyjb-hockey-game.jpg"
+            width={250}
+            height={250}
+            alt="abby at a NHL hockey game"
+          />
       </div>
-      <div className="p-2 rounded-lg bg-beige shadow-lg shadow-blue">
+      <div className="p-2 rounded-lg bg-beige min-w-[300px]">
+        <p className="mb-4 text-lg text-purple">
+          Hello, thanks for stopping by!
+        </p>
+        <p className="mb-4 text-lg text-purple">
+          I am a software engineer with five years of experience working remotely
+          out of Henderson, NV. At my current position at Greenhouse Software I have
+          honed my skills in critical thinking, cross-functional communication, and
+          technical expertise within the Ruby on Rails and React technical stack.
+          I've led projects that have increased the efficiency for our customers at scale. 
+          From a budding thought to a built solution, I dig deep to find the best outcome 
+          with performance, useability and maintainability in mind!
+        </p>
         <p className="text-lg text-purple">
-          I'm Abigail Bashore, a fullstack software engineer 
+          Outside of work I train for half marathons, watch NHL hockey with my family, and
+          enjoy playing video games and travelling with friends.
         </p>
       </div>
     </main>
