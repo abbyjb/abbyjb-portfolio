72a5dd600454e9f82f6fd720c09e5049d22fcd98
Abby Bashore <abbyjbdev@gmail.com>
2024-08-19 20:08:18 -0700

    use new colors

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <main className="flex items-center justify-center">
-      <div className="bg-beige m-2 p-2 rounded-lg shadow-lg shadow-blue text-center">
-        <h2 className="text-4xl text-center text-purple">404 Not Found</h2>
-        <Link className="text-purple text-lg underline hover:text-green" href="/">Go back home</Link>
+    <main className="flex items-center justify-center min-h-[85vh]">
+      <div className="m-2 p-2 rounded-lg  text-center">
+        <h2 className="text-4xl text-center mb-4">404 Not Found</h2>
+        <Link className="text-purple text-lg hover:underline hover:text-sunglow-300" href="/">Go back home</Link>
       </div>
     </main>
   );
