136d23e6ae1e4d2d0ef2389a3eae55fa2909615f
abbyjb <22553870+abbyjb@users.noreply.github.com>
2025-11-17 19:37:07 -0800

    make page more responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navigation from '@/app/components/navigation'
 
 export default function Home() {
   return (
+    <main className="md:grid md:gap-3 md:grid-cols-4 md:grid-rows-layout min-h-screen w-[90vw] mx-auto my-5">
       <Navigation />  
       <Image
           className="rounded-lg my-4"
@@ -15,7 +16,7 @@ export default function Home() {
           height={300}
           alt="abby in nyc"
       />
-      <div className="p-2 m-2 h-fit w-fit col-span-3 rounded-lg bg-off-white">
+      <div className="p-2 my-4 h-fit md:col-span-3 rounded-lg bg-lighter-beige">
         <p className="text-lg text-purple">
           Software engineer with over five years of experience in building maintainable and performant software solutions. 
           Strengths in delivering well architected and tested code. Provides concise and actionable updates. 
