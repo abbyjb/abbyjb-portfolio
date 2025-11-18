04ac6d021d04af205a937ccb2f55bed8c7a75c85
abbyjb <22553870+abbyjb@users.noreply.github.com>
2025-11-17 19:37:07 -0800

    about me corrections

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,9 @@ export default function Home() {
           out of Henderson, NV. At my current position at Greenhouse Software I have
           honed my skills in critical thinking, cross-functional communication, and
           technical expertise within the Ruby on Rails and React technical stack.
-          I've led projects that have increased the efficiency for our customers at scale. 
+          I&apos;ve led projects that have increased the efficiency for our customers at scale. 
           From a budding thought to a built solution, I dig deep to find the best outcome 
-          with performance, useability and maintainability in mind!
+          with performance, usability, and maintainability in mind!
         </p>
         <p className="text-lg text-purple">
           Outside of work I train for half marathons, watch NHL hockey with my family, and
