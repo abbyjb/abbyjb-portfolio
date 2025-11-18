90efceddf5fc1d45071c02a1d7ea7f1a4550560e
abbyjb <22553870+abbyjb@users.noreply.github.com>
2025-11-17 19:37:07 -0800

    add another image and style with grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,34 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col md:flex-row items-start justify-between gap-3">
-      <Image
-          className="rounded-lg shadow-xl shadow-blue"
+    <main className="sm:grid sm:grid-cols-2 flex flex-col items-center justify-between gap-3">
+      <div className="relative">
+        <Image
+          className="sm:absolute sm:top-0 sm:left-0 my-3 rounded-full shadow-xl shadow-blue"
           src="/abbyjb-in-nyc.jpg"
-          style={{
-            width: '100%',
-            height: 'auto',
-          }}
+          // style={{
+          //   width: '100%',
+          //   height: 'auto',
+          // }}
           width={300}
           height={300}
           alt="abby in nyc"
-      />
+        />
+        <Image 
+          className="sm:absolute sm:top-40 sm:left-40 my-3 rounded-full shadow-xl shadow-blue"
+          src="/abbyjb-running-5k.jpg"
+          // style={{
+          //   width: '100%',
+          //   height: 'auto',
+          // }}
+          width={300}
+          height={300}
+          alt="abby running in a 5k"
+        />
+      </div>
       <div className="p-2 rounded-lg bg-beige shadow-lg shadow-blue">
         <p className="text-lg text-purple">
-          Software engineer with over five years of experience in building maintainable and performant software solutions. 
-          Strengths in delivering well architected and tested code. Provides concise and actionable updates. 
-          Collaborates with cross-functional teams in a remote environment. 
-          Proficient in full-stack development in Ruby on Rails, React, PostgreSQL, and AWS.
+          I'm Abigail Bashore, a fullstack software engineer 
         </p>
       </div>
     </main>
